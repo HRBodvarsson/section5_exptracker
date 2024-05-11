dde1d1f13770c283ba295c040a8a6e5db885d431
HR Bodvarsson <hrbodvarsson@gmail.com>
2024-05-11 22:14:44 +0000

    first commit

---
+++ android/app/src/main/kotlin/com/example/section5_exptracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.section5_exptracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:section5_exptracker/expenses.dart';
+
+void main() {
+  runApp(
+  MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
